1564eb7b81c27a2d74aea667b0862d01b82a49a8
Dave Bray <david@brayworth.com.au>
2020-08-28 10:50:42 +1000

    tweak

--- c19/views/settings.php
+++ c19/views/settings.php
@@ -17,10 +17,10 @@ use strings;  ?>
   <form id="<?= $_form = strings::rand() ?>" autocomplete="off">
     <input type="hidden" name="action" value="save-settings">
     <div class="modal fade" tabindex="-1" role="dialog" id="<?= $_modal = strings::rand() ?>" aria-labelledby="<?= $_modal ?>Label" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header bg-secondary text-white py-2">
-            <h5 class="modal-title" id="<?= $_modal ?>Label">Title</h5>
+            <h5 class="modal-title" id="<?= $_modal ?>Label">Settings</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
